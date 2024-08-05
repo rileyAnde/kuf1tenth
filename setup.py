a53c3e3a0eb79393504c61cf88bf27ff54b9185e
rileyAnde <randerson1@ku.edu>
2024-08-05 12:19:03 -0500

    added brake node and wall follow test

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'kuf1tenth'
 
@@ -10,6 +12,9 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+        (os.path.join('share', package_name, 'map'), glob('map/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,6 +25,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'brake = kuf1tenth.brake:main',
+            'wall_follow = kuf1tenth.wall_follow:main'
         ],
     },
 )
