564f457ae35ebf22968b9cf38a5b28ddee1497a6
rileyAnde <143535501+rileyAnde@users.noreply.github.com>
2024-08-06 10:45:34 -0500

    update solver.py to include centerline calculation

--- solver.py
+++ solver.py
@@ -2,10 +2,11 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import csv
+import math
 
 # load the track from png
 map_path = 'maps\\'
-map_file = 'Spielberg_map'  # Replace with your map file
+map_file = 'Spielberg_map'  #Replace with the map file
 image = cv2.imread(map_path + map_file + '.png', cv2.IMREAD_GRAYSCALE)
 
 if image is None:
@@ -23,10 +24,10 @@ inner_contour = None
 
 for i, contour in enumerate(contours):
     if cv2.contourArea(contour) > 100:  # ignore small contours (scan error, etc)
-        if outer_contour is None:
-            outer_contour = contour
+        if inner_contour is None:
+            inner_contour = contour
         else:
-            inner_contour = contour  # the next largest contour will be the inner one
+            outer_contour = contour  # the next largest contour will be the inner one
 
 # ensure both inside and outside lines are found
 if outer_contour is None:
@@ -36,15 +37,45 @@ if inner_contour is None:
 
 
 # calculate the racing line here!
-centerline = (outer_contour - inner_contour)
+def dist(pt1, pt2):
+    return math.sqrt((pt1[0]-pt2[0])**2 + (pt1[1]-pt2[1])**2)
+
+
+#centerline calc
+def calc_centerline(inner, outer):
+    inner_waypoints = []
+    outer_waypoints = []
+    centerline = []
+    nearest_pt = [0,0]
+    pt_dist = 9999999999
+    for waypoint in inner:
+        x, y = waypoint[0]
+        inner_waypoints.append([x,y])
+    for waypoint in outer:
+        x,y = waypoint[0]
+        outer_waypoints.append([x,y])
+    print(inner_waypoints)
+    for i in range(len(inner_waypoints)):
+        nearest_pt = [0,0]
+        pt_dist = 9999999999
+        for x in range(len(outer_waypoints)):
+            new_pt_dist = dist(inner_waypoints[i], outer_waypoints[x])
+            if new_pt_dist < pt_dist:
+                pt_dist = new_pt_dist
+                nearest_pt = outer_waypoints[x]
+        centerline.append([((inner_waypoints[i][0]+nearest_pt[0])//2),((inner_waypoints[i][1]+nearest_pt[1])//2)])
+        print(centerline[i])
+    return centerline
+
+
 
 # save waypoints to a CSV file
 csv_file_path = 'paths\\' + map_file + '.csv'
 with open(csv_file_path, mode='w', newline='') as csv_file:
     csv_writer = csv.writer(csv_file)
     csv_writer.writerow(['x', 'y'])  # header
-    for waypoint in centerline:
-        x, y = waypoint[0]
+    centerline = calc_centerline(inner_contour, outer_contour)
+    for x,y in centerline:
         csv_writer.writerow([x, y])
 
 print(f'Waypoints saved to {csv_file_path}')
@@ -57,7 +88,7 @@ if inner_contour is not None:
     cv2.drawContours(visualization_image, [inner_contour], -1, (255, 0, 0), 2)
 
 for waypoint in centerline:
-    cv2.circle(visualization_image, tuple(waypoint[0]), 3, (0, 0, 255), -1)
+    cv2.circle(visualization_image, tuple(waypoint), 3, (0, 0, 255), -1)
 
 plt.imshow(cv2.cvtColor(visualization_image, cv2.COLOR_BGR2RGB))
 plt.title('Racing Line')
