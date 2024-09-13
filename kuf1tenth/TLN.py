a09438f43412a5886df5a38e5d5413f9d7059fa2
Riley Anderson <143535501+rileyAnde@users.noreply.github.com>
2024-09-13 18:21:04 -0500

    add TLN

---
+++ kuf1tenth/TLN.py
@@ -0,0 +1,76 @@
+import rclpy
+import numpy as np
+import time
+import tensorflow as tf
+from rclpy.node import Node
+from ackermann_msgs.msg import AckermannDriveStamped
+from sensor_msgs.msg import LaserScan
+from std_msgs.msg import Header
+
+class TLNNode(Node):
+    def __init__(self):
+        super().__init__('tln_node')
+        self.ackermann_publisher = self.create_publisher(AckermannDriveStamped, '/drive', 10)
+        self.scan_subscription = self.create_subscription(LaserScan, '/scan', self.scan_callback, 10)
+        self.get_logger().info('TLNNode has been started.')
+
+        self.model_path = "/home/r478a194/Downloads/f1_tenth_model_small_noquantized.tflite"
+        self.interpreter = tf.lite.Interpreter(model_path=self.model_path)
+        self.interpreter.allocate_tensors()
+        self.input_index = self.interpreter.get_input_details()[0]["index"]
+        self.output_index = self.interpreter.get_output_details()[0]["index"]
+        self.scan_buffer = np.zeros((2, 20))
+
+    def linear_map(self, x, x_min, x_max, y_min, y_max):
+        return (x - x_min) / (x_max - x_min) * (y_max - y_min) + y_min    
+
+    def scan_callback(self, msg):
+        scans = np.array(msg.ranges)
+        scans = np.append(scans, [20])
+        self.get_logger().info(f'num scans:{len(scans)}')
+        noise = np.random.normal(0, 0.5, scans.shape)
+        scans = scans + noise
+        scans[scans > 10] = 10
+        scans = scans[::2]  # Use every other value
+        scans = np.expand_dims(scans, axis=-1).astype(np.float32)
+        scans = np.expand_dims(scans, axis=0)
+
+        self.interpreter.set_tensor(self.input_index, scans)
+        start_time = time.time()
+        self.interpreter.invoke()
+        inf_time = (time.time() - start_time) * 1000  # in milliseconds
+        self.get_logger().info(f'Inference time: {inf_time:.2f} ms')
+
+        output = self.interpreter.get_tensor(self.output_index)
+        steer = output[0, 0]
+        speed = output[0, 1]
+
+        min_speed = 1
+        max_speed = 8
+        speed = self.linear_map(speed, 0, 1, min_speed, max_speed)
+
+        self.publish_ackermann_drive(speed, steer)
+
+    def publish_ackermann_drive(self, speed, steering_angle):
+        ackermann_msg = AckermannDriveStamped()
+        ackermann_msg.header = Header()
+        ackermann_msg.header.stamp = self.get_clock().now().to_msg()
+        ackermann_msg.drive.speed = float(speed)
+        ackermann_msg.drive.steering_angle = float(steering_angle)
+
+        self.ackermann_publisher.publish(ackermann_msg)
+        self.get_logger().info(f'Published AckermannDriveStamped message: speed={speed}, steering_angle={steering_angle}')
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = TLNNode()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        node.get_logger().info('Keyboard Interrupt (SIGINT)')
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
---
+++ kuf1tenth/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+import os
+from glob import glob
+
+package_name = 'kuf1tenth'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+        (os.path.join('share', package_name, 'map'), glob('map/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='root',
+    maintainer_email='root@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'brake = kuf1tenth.brake:main',
+            'wall_follow = kuf1tenth.wall_follow:main',
+            'old_ftg = kuf1tenth.gap:main',
+            'ftg = kuf1tenth.ftg:main',
+            'translate = kuf1tenth.translate:main',
+            'tln = kuf1tenth.TLN:main'
+        ],
+    },
+)
